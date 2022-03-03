37381be89d36144493290cdcda47ade4b726e199
JacobShiber <yaakovshiber@gmail.com>
2022-03-03 12:35:45 +0200

    another try

--- server/server.js
+++ server/server.js
@@ -23,9 +23,9 @@ app.use('/workers', passport.authenticate('jwt', { session: false }), workers);
 //     res.send('Server is online');
 // })
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV == 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get('/', (req, res) =>
+    app.get('*', (req, res) =>
         res.sendFile(path.join(_dirname, '../client/build', 'index.html'))
     )
 }
