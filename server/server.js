3c069036822727fdae67133749835e70f976d07b
JacobShiber <yaakovshiber@gmail.com>
2022-03-03 13:46:05 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use('/workers', passport.authenticate('jwt', { session: false }), workers);
 app.listen(port);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get('*', (req, res) =>
+    app.get('/', (req, res) =>
         res.sendFile(path.join(_dirname, '../client/build', 'index.html'))
     )
 }
